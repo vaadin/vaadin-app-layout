0e16c405a6216b670632d5696515f934197798d7
web-padawan <iamkulykov@gmail.com>
2020-02-04 18:43:53 +0200

    docs: add API docs with api-viewer

--- src/vaadin-app-layout.ts
+++ src/vaadin-app-layout.ts
@@ -4,7 +4,35 @@ import { AppLayoutMixin } from './vaadin-app-layout-mixin';
 
 /**
  * `<vaadin-app-layout>` is a Web Component providing a quick and easy way to get a common
- * application layout structure done.
+ * application layout structure done. For best results, please take the following recommendations
+ * into account:
+ *
+ *  1. add the component to the root level of your application (as a direct child of `<body>`),
+ *  2. set `height: 100%` on both `html` and `body` to make the layout expand to full page height,
+ *  3. include a viewport meta tag which contains `viewport-fit=cover` to the page as follows:
+ * ```
+ * <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+ * ```
+ *
+ * @attr {boolean} overlay - Attribute set when layout has drawer overlay on mobile devices.
+ *
+ * @csspart backdrop - Overlay displayed when opening drawer on mobile devices
+ * @csspart drawer - Container for the drawer area
+ * @csspart navbar - Container for the navigation bar (top and bottom)
+ * @csspart navbar-bottom - Container for the bottom navigation bar
+ * @csspart content - Container for the page content
+ *
+ * @slot - Default slot for the page content
+ * @slot navbar - Slot for the top navbar area
+ * @slot drawer - Slot for an application menu
+ * @slot touch-optimized - Slot for the bottom navbar area (only visible on mobile devices)
+ *
+ * @event close-overlay-drawer - Event that can be used to close drawer programmatically.
+ * @event drawer-opened-changed - Fired when the `drawerOpened` property changes.
+ * @event overlay-changed - Fired when the `overlay` property changes.
+ * @event primary-section-changed - Fired when the `primarySection` property changes.
+ * @event touch-optimized-changed - Fired when the `touchOptimized` property changes.
+ * @event resize - Fired when the element is resized. Non-bubbling.
  */
 @customElement('vaadin-app-layout')
 export class VaadinAppLayout extends AppLayoutMixin(AppLayoutBase) {
