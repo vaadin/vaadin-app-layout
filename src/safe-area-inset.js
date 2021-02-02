d6528ce653a53eaaecc34d92718f304e0fc80601
web-padawan <iamkulykov@gmail.com>
2021-02-02 09:50:21 +0200

    Remove custom-style usage

--- src/safe-area-inset.js
+++ src/safe-area-inset.js
@@ -1,9 +1,7 @@
-import '@polymer/polymer/lib/elements/custom-style.js';
 const $_documentContainer = document.createElement('template');
 
-$_documentContainer.innerHTML = `<custom-style>
+$_documentContainer.innerHTML = `
   <style>
-    /* stylelint-disable length-zero-no-unit */
     /* Use units so that the values can be used in calc() */
     html {
       --safe-area-inset-top: constant(safe-area-inset-top, 0px);
@@ -20,8 +18,7 @@ $_documentContainer.innerHTML = `<custom-style>
         --safe-area-inset-left: env(safe-area-inset-left, 0px);
       }
     }
-    /* stylelint-enable */
   </style>
-</custom-style>`;
+`;
 
 document.head.appendChild($_documentContainer.content);
